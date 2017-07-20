105f6d0193aa5d315727363bdadcb58ad2359447
Norman Carcamo <normancarcamo@gmail.com>
2017-07-20 10:47:32 -0600

    fixed props in renderComponent function

--- src/_helpers.js
+++ src/_helpers.js
@@ -95,7 +95,7 @@ function renderComponent(location, component, props) {
       console.info('The component you\'re trying to render seems to not exists.');
     }
   } else {
-    Component = React.createElement(component, JSON.parse(avoidXSS(props)));
+    Component = React.createElement(component, JSON.parse(avoidXSS(props.props)));
   }
 
   let context = {};
