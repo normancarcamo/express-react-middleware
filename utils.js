d60d444c1fad2929f8eab2a5880979762b5c2b6a
Norman Carcamo <normancarcamo@gmail.com>
2017-09-29 17:04:34 -0600

    added "getRoute" function as a helper

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const helpers = require(__dirname+'/src/helpers.js');
 
 module.exports.getComponentFromRoutes = helpers.getComponentFromRoutes;
+module.exports.getRoute = helpers.getComponentByPathname;
 module.exports.syncRouter = helpers.syncRouter;
